a708efbf6ba0c729906ebb5b63ecbb9467be1a49
hippopoo255 <hippopoo255>
2023-01-27 16:03:39 +0900

    feat: restruct-pager

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -1,14 +1,23 @@
 window.onload = function () {
-  //<editor-fold desc="Changeable Configuration Block">
-  window.ui = SwaggerUIBundle({
-    dom_id: "#swagger-ui",
-    deepLinking: true,
-    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
-    layout: "StandaloneLayout",
-    queryConfigEnabled: false,
-    url: "../openapi.app.yaml",
-  });
+  
+      
+      //<editor-fold desc="Changeable Configuration Block">
+      window.ui = SwaggerUIBundle({
+        "dom_id": "#swagger-ui",
+        deepLinking: true,
+        presets: [
+          SwaggerUIBundle.presets.apis,
+          SwaggerUIStandalonePreset
+        ],
+        plugins: [
+          SwaggerUIBundle.plugins.DownloadUrl
+        ],
+        layout: "StandaloneLayout",
+        queryConfigEnabled: false,
+        url: "../openapi.app.yaml",
+      })
+      
+      //</editor-fold>
 
-  //</editor-fold>
+
 };
