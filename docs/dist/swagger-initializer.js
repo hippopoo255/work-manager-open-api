0dd568566af6afc69a6e182ea657e1460806a076
hippopoo255 <hippopoo255>
2023-01-27 14:10:55 +0900

    feat: replace request bodies to models

--- docs/dist/swagger-initializer.js
+++ docs/dist/swagger-initializer.js
@@ -1,27 +1,14 @@
 window.onload = function () {
-  
-      
-      
-      
-      //<editor-fold desc="Changeable Configuration Block">
-      window.ui = SwaggerUIBundle({
-        "dom_id": "#swagger-ui",
-        deepLinking: true,
-        presets: [
-          SwaggerUIBundle.presets.apis,
-          SwaggerUIStandalonePreset
-        ],
-        plugins: [
-          SwaggerUIBundle.plugins.DownloadUrl
-        ],
-        layout: "StandaloneLayout",
-        queryConfigEnabled: false,
-        url: "../openapi.app.yaml",
-      })
-      
-      //</editor-fold>
+  //<editor-fold desc="Changeable Configuration Block">
+  window.ui = SwaggerUIBundle({
+    dom_id: "#swagger-ui",
+    deepLinking: true,
+    presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "StandaloneLayout",
+    queryConfigEnabled: false,
+    url: "../openapi.app.yaml",
+  });
 
-
-
-
+  //</editor-fold>
 };
